ff2363033bc907e7bd639d212893c61dc10a5c6c
Егор В <egorikes3@gmail.com>
2020-10-07 12:37:06 +0300

    L02_HomeWork_1 added

---
+++ 02/HomeWork/L02_HomeWork_1/L02_HomeWork_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace L02_HomeWork_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Введите число 1: ");
+			string Input = Console.ReadLine();
+			float Value1 = float.Parse(Input);
+			Console.Write("Введите число 2: ");
+			Input = Console.ReadLine();
+			float Value2 = float.Parse(Input);
+			Console.WriteLine($"Сумма {Value1 + Value2}");
+			Console.WriteLine($"Разность {Value1 - Value2}");
+			Console.WriteLine($"Частное {Value1 / Value2}");
+			Console.WriteLine($"Произведение {Value1 * Value2}");
+			Console.ReadKey();
+
+		}
+	}
+}
