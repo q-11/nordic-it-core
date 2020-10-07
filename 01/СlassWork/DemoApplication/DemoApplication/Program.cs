1980ebff751cbccca30b24e94a863480a7b35305
Егор В <egorikes3@gmail.com>
2020-10-07 12:28:44 +0300

    L01_HW1 + CW1

---
+++ 01/СlassWork/DemoApplication/DemoApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace DemoApplication
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Введите ваше имя: ");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine("Привет " + name + "!");
+            Console.ReadLine();
+        }
+    }
+}
