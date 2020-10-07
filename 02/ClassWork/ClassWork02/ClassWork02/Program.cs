6e74d35fb1618f2ae3ba3f4ed523897876a517b0
Егор В <egorikes3@gmail.com>
2020-10-07 12:33:30 +0300

    L02_ClassWork added

---
+++ 02/ClassWork/ClassWork02/ClassWork02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Dynamic;
+
+namespace ClassWork02
+{
+	class Program
+	{
+			static void Main(string[] args)
+		{
+			//char letter = 'A';
+			//Console.WriteLine(letter);
+			//string name = "Bob";
+			//Console.WriteLine(name);
+			//var i = 0x0000001;
+			//float pi = 3.1415f;
+			//Console.WriteLine(i + pi);
+			//int parseValue = int.Parse("232323");
+			//parseValue = parseValue + 1;
+			//Console.WriteLine(parseValue);
+			Console.Write("Введите ваш возраст: ");
+			string inputAge = Console.ReadLine();
+			int age = int.Parse(inputAge);
+			Console.WriteLine($"Ваш возраст через 10 лет: {age + 10}");
+		}
+	}
+}
