d4496efeee7e6d74729f5d9394097067c2d5d46b
Егор В <egorikes3@gmail.com>
2020-10-11 12:24:46 +0300

    L03_HomeWork_1 added

---
+++ 03/HomeWork/L03_HomeWork_1/L03_HomeWork_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace L03_HomeWork_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] names = new string[3];
+			int[] age = new int[3];
+			for (int i = 0; i < names.Length; i++)
+			{
+				Console.Write($"Enter {i + 1} name ");
+				names[i] = Console.ReadLine();
+				Console.WriteLine($"How old is {names[i]} ?");
+				age[i] = int.Parse(Console.ReadLine());
+			}
+			for (int i = 0; i < names.Length; i++)
+			{
+				Console.WriteLine($"Name: {names[i]}" + $" age in 4 years: {age[i] + 4}");
+			}
+		}
+	}
+}
