156ba2545314ef0f9d2bff683329a92d4946024d
Егор В <egorikes3@gmail.com>
2020-10-17 11:38:22 +0300

    L03_HomeWork_2 correction

--- 03/HomeWork/L03_HomeWork_2/L03_HomeWork_2/Program.cs
+++ 03/HomeWork/L03_HomeWork_2/L03_HomeWork_2/Program.cs
@@ -9,12 +9,16 @@ namespace L03_HomeWork_2
 		{
 			int[] sourceFactors1 = new int[10];
 			int[] sourceFactors2 = new int[sourceFactors1.Length];
+
 			for (int i = 0; i < sourceFactors1.Length; i++)
 			{
 				sourceFactors1[i] = i + 1;
 				sourceFactors2[i] = i + 1;
 			}
-			for (int i1 = 0; i1 < sourceFactors1.Length; i1++) {
+
+			for (int i1 = 0; i1 < sourceFactors1.Length; i1++)
+			{
+
 				for (int i2 = 0; i2 < sourceFactors1.Length; i2++)
 				{
 					int outPut;
@@ -26,8 +30,10 @@ namespace L03_HomeWork_2
 					else if (outPut >= 100)
 						Console.Write(outPut + " ");
 				}
+
 				Console.WriteLine();
 			}
+
 		}
 	}
 }
