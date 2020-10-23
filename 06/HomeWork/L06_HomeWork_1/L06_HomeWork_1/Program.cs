2d118ddc0afe62dfc0c1e32d2cc175395af42659
Егор В <egorikes3@gmail.com>
2020-10-23 19:03:48 +0300

    L06_HomeWork added

---
+++ 06/HomeWork/L06_HomeWork_1/L06_HomeWork_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace L06_HomeWork_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Введите положительное натуральное число не более 2 миллиардов: ");
+			while (true)
+			{
+				string input = Console.ReadLine();
+				int number;
+				bool success = Int32.TryParse(input, out number);
+				if (success == true && Convert.ToInt32(input) > 0)
+				{
+					char[] numbers = new char [input.Length];
+					number = 0;
+					for (int i = 0; i < input.Length; i++)
+					{
+						numbers[i] = input[i];
+						if (numbers[i] % 2 == 0)
+						{
+							number++;
+						}
+					}
+					Console.WriteLine($"Четных чисел: {number}");
+					break;
+				}
+				else
+					Console.Write("Вы ввели что-то не то! Попробуйте снова: ");
+			}
+		}
+	}
+}
