79dd3615d75a332f29a1fb834e436fc2df3bb4cd
Егор В <egorikes3@gmail.com>
2020-10-29 00:24:00 +0300

    L07_HomeWork_2 added

---
+++ 07/HomeWork/L07_HomeWork_2/L07_HomeWork_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace L07_HomeWork_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string line = Console.ReadLine();
+			char[] lettersOfLine = line.ToLower().ToCharArray();
+
+			for (int i = lettersOfLine.Length - 1; i >= 0; i--)
+			{
+				Console.Write(lettersOfLine[i]);
+			}
+
+		}
+	}
+}
