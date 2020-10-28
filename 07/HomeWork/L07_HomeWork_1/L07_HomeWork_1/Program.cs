3e4f4e2494c2b135b59c3a824fc2e498c52382e3
Егор В <egorikes3@gmail.com>
2020-10-29 00:23:15 +0300

    L07_HomeWork_1 added

---
+++ 07/HomeWork/L07_HomeWork_1/L07_HomeWork_1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace L07_HomeWork_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите строку из нескольких слов:");
+			string[] words;
+
+			do
+			{
+				string input = Console.ReadLine();
+				words = input.ToLower().Split(' ');
+				int number = 0;
+				bool succses = Int32.TryParse(input, out number);
+
+				if (succses == true)
+				{
+					Console.WriteLine("Вы ввели число! Попробуйте еще раз:");
+					continue;
+				}
+				
+				else if (words.Length < 2)
+				{
+					Console.WriteLine("Слишком мало слов :( Попробуйте ещё раз:");
+					continue;
+				}
+
+				else
+				{
+					number = 0;
+
+					for (int i = 0; i < words.Length; i++)
+					{
+						if (words[i].StartsWith("а"))
+						{
+							number++;
+						}
+					}
+
+					Console.WriteLine($"Количество слов, начинающихся с буквы 'А': " + number);
+					break;
+				}
+
+			} while (true);
+
+		}
+	}
+}
