d53b25e4314695a764591c49b44120635338afca
Егор В <egorikes3@gmail.com>
2020-11-15 15:01:41 +0300

    L10_HomeWork_1 added

---
+++ 10/HomeWork/L10_HomeWork_1/L10_HomeWork_1/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace L10_HomeWork_1
+{
+	class Person
+	{
+		private byte _age;
+
+		public string Name;
+
+		public byte Age
+		{
+			get { return _age; }
+			set
+			{
+				if (value == 0 || value >= 150)
+				{
+					throw new Exception("Age should be less or equals to 150");
+				}
+
+				_age = value;
+			}
+		}
+		public byte AgeInFourYears
+		{
+			get
+			{ return (Age += 4); }
+		}
+
+		public string PropertiesString
+		{
+			get
+			{
+				return $"Name: {Name}, age in 4 years: {AgeInFourYears}";
+			}
+		}
+	}
+}
---
+++ 10/HomeWork/L10_HomeWork_1/L10_HomeWork_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Cache;
+
+namespace L10_HomeWork_1
+{
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const byte number = 3;
+			Person[] persons = new Person[number];
+			for (int i = 0; i < persons.Length; i++)
+			{
+				persons[i] = new Person();
+				Console.Write($"Enter name {i + 1}: ");
+				string input = Console.ReadLine();
+				persons[i].Name = input;
+
+				Console.Write($"Enter age {i + 1}: ");
+
+				do
+				{
+
+					try
+					{
+						input = Console.ReadLine();
+						persons[i].Age = Byte.Parse(input);
+					}
+					catch (Exception e)
+					{
+						Console.WriteLine(e.Message + " Try again:");
+					}
+				} while (persons[i].Age == 0);
+
+			}
+
+			for (int i = 0; i != persons.Length; i++)
+			{
+				Console.WriteLine(persons[i].PropertiesString);
+			}
+		}
+	}
+}
