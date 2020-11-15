e6215a39b3cd01facda2664cd6e24763625d399a
Егор В <egorikes3@gmail.com>
2020-11-15 15:05:45 +0300

    L09_ClassWork added

---
+++ 09/ClassWork/L09_ClassWork_1/L09_ClassWork_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace L09_ClassWork_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string input1 = Console.ReadLine();
+			string input2 = Console.ReadLine();
+			int sameLettrsNumber = 0;
+
+			for (int i = 0; i < input1.Length; i++)
+			{
+				for (int j = 0; j < input2.Length; j++)
+				{
+					if(input1[i] == input2[j])
+					{
+						sameLettrsNumber++;
+					}
+				}
+			}
+			Console.WriteLine(sameLettrsNumber);
+		}
+	}
+}
---
+++ 09/ClassWork/L09_ClassWork_2/L09_ClassWork_2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+
+namespace L09_ClassWork_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const int length = 20;
+			const int maxValue = 100;
+			Stopwatch stopwatch = new Stopwatch();
+
+			int[] arraySource = GetSourceArray(length, maxValue);
+			int[] arraySortedBubble = GetArraySortedBubble(arraySource);
+
+			Stopwatch.StartNew()
+			int[] arraySortedInternal = GetArraySortedBubble(arraySource);
+			Stopwatch
+
+			stopwatch.Start();
+			BubbleSort(bubbleSortedArray);
+			stopwatch.Stop();
+
+
+			WriteOutArray(arraySource);
+			WriteOutArray(arraySortedBubble);
+		}
+
+		public static int[] GetSourceArray(int length, int maxValue)
+		{
+			int[] array = new int[length];
+			var random = new Random();
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				array[i] = random.Next(maxValue);
+			}
+
+			return array;
+		}
+
+		public static void WriteOutArray(int[] array)
+		{
+			Console.WriteLine(string.Join(", ", array));
+		}
+
+		public static int[] GetArraySortedBubble(int[] sourceArray)
+		{
+			int[] array = (int[])sourceArray.Clone();
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				int limit = array.Length - 1 - i;
+
+				for (int j = 0; j < limit; j++)
+				{
+					if (array[j] > array[j + 1])
+					{
+						int temp = array[j + 1];
+						array[j + 1] = array[j];
+						array[j] = temp;
+					}
+				}
+
+			}
+
+			return array;
+		}
+	}
+}
