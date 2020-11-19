d8f0b849286a46925c10eea3c1d5230c42ef2a86
Егор В <egorikes3@gmail.com>
2020-11-20 01:26:28 +0300

    L11_HomeWork_1 added

---
+++ 11/HomeWork/L11_HomeWork_1/L11_HomeWork_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace L11_HomeWork_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ReminderItem exemplier1 = new ReminderItem(DateTimeOffset.Parse("2020-11-20 01:11:00.0000"), "get up!");
+			ReminderItem exemplier2 = new ReminderItem(DateTimeOffset.Parse("2020-11-25 18:00:00.0000"), "lesson!");
+
+			exemplier1.WriteProperties();
+			exemplier2.WriteProperties();
+
+		}
+	}
+}
---
+++ 11/HomeWork/L11_HomeWork_1/L11_HomeWork_1/ReminderItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace L11_HomeWork_1
+{
+	class ReminderItem
+	{
+		public DateTimeOffset AlarmDate;
+		public string AlarmMessage;
+		public TimeSpan TimeToAlarm
+		{
+			get
+			{
+				return AlarmDate.Subtract(DateTimeOffset.Now);
+			}
+		}
+		public bool IsOutdated
+		{
+			get
+			{
+				if (TimeToAlarm	 <= TimeSpan.Zero)
+				{
+					return true;
+				}
+				return false;
+			}
+		}
+		public ReminderItem(DateTimeOffset date, string message)
+		{
+			AlarmDate = date;
+			AlarmMessage = message;
+		}
+		public void WriteProperties()
+		{
+			Console.WriteLine($"AlarmDate: {AlarmDate}");
+			Console.WriteLine($"AlarmMessage: {AlarmMessage}");
+			Console.WriteLine($"TimeToAlarm: {TimeToAlarm}");
+			Console.WriteLine($"IsOutdated: {IsOutdated}");
+		}
+	}
+}
