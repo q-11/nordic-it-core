91818cc101247d0d877b49f28279a4c7fb76d8aa
Егор В <egorikes3@gmail.com>
2020-11-25 19:41:23 +0300

    L12_ClassWork added

---
+++ 12/ClassWork/inheritance_example/inheritance_example/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace inheritance_example
+{
+	public class Person
+	{
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public DateTimeOffset Birthday { get; set; }
+		public byte Age
+		{
+			get
+			{
+				return GetAge();
+			}
+		}
+		public Person(string name, string surname, DateTimeOffset birthday)
+		{
+			Name = name;
+			Surname = surname;
+			Birthday = birthday;
+		}
+		public byte GetAge()
+		{
+			TimeSpan liveTime = DateTimeOffset.Now.Subtract(Birthday);
+			byte years = (byte)Math.Floor(liveTime.TotalDays / 365.242);
+			return years;
+		}
+		public void WriteDescription()
+		{
+			Console.WriteLine($"{Name} {Surname} родился {Birthday.ToString("dd.MM.yyyy")}. На данный момент {Age} лет.");
+		}
+	}
+}
---
+++ 12/ClassWork/inheritance_example/inheritance_example/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace inheritance_example
+{
+	class Employee : Person
+	{
+		public int Code { get; set; }
+		public Employee(int code, string name, string surname, DateTimeOffset birthday) : base(name, surname, birthday)
+		{
+			Code = code;
+		}
+		public new void WriteDescription()
+		{
+			Console.WriteLine($"{Code}: {Name} {Surname} родился {Birthday.ToString("dd.MM.yyyy")}. На данный момент {Age} лет.");
+		}
+
+
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var person = new Person("Егор", "Верховцев", DateTimeOffset.Parse("2002-06-16"));
+			var employee = new Employee(0001, "Егор", "Верховцев", DateTimeOffset.Parse("2001-06-16"));
+
+			person.WriteDescription();
+			employee.WriteDescription();
+		}
+
+	}
+}
